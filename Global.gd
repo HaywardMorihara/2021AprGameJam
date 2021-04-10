c38d389e1e76a5bc194c0dafaf1af7e1d3d5a200
HaywardMorihara <nathanielmorihara@gmail.com>
2021-04-10 18:32:11 -0400

    Timer start after intro

--- Global.gd
+++ Global.gd
@@ -2,9 +2,3 @@ extends Node
 
 
 var next_exit_map_position : Vector2
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	# TODO Make this trigger by finishing opening conversation
-	GlobalTimer.start()
--- cutscene/Intro.gd
+++ cutscene/Intro.gd
@@ -3,4 +3,5 @@ extends Control
 
 func _input(event):
 	if event is InputEventKey and event.pressed or Input.is_action_pressed("next"):
+		GlobalTimer.start()
 		get_tree().change_scene("res://house/InteriorHouse.tscn")
