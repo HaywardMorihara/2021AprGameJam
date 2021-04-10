afe306dcadb92dd200c698f6eca9835af86999de
HaywardMorihara <nathanielmorihara@gmail.com>
2021-04-10 07:56:50 -0400

    Rename interor ouse

--- cutscene/Intro.gd
+++ cutscene/Intro.gd
@@ -3,4 +3,4 @@ extends Control
 
 func _input(event):
 	if event is InputEventKey and event.pressed or Input.is_action_pressed("next"):
-		get_tree().change_scene("res://house/PlayerHouse.tscn")
+		get_tree().change_scene("res://house/InteriorHouse.tscn")
