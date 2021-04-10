76a152b3ce6085130d663d4a673e9f6af77cf4fa
HaywardMorihara <nathanielmorihara@gmail.com>
2021-04-10 10:11:17 -0400

    House on map, enter/exit

--- house/StaticBody2D.gd
+++ house/StaticBody2D.gd
@@ -1,22 +1,11 @@
 extends Area2D
 
+export (String) var destination_scene
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+# TODO Require an export var?
 
 func _on_Location_body_entered(body):
 	#	https://godotengine.org/qa/65414/how-to-tell-what-sort-of-body-ive-hit-in-area-body_entered
 	if body.name == "PlayerBody":
-		get_tree().change_scene("res://map/Map.tscn")
+		get_tree().change_scene(destination_scene)
+		
