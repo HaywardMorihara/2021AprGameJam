39e3e9edab56c54dc8ff39006daa6047f70942de
HaywardMorihara <nathanielmorihara@gmail.com>
2021-04-11 17:50:12 -0400

    Reset stillness timer on player input

--- player/Player.gd
+++ player/Player.gd
@@ -5,18 +5,23 @@ export var speed = 200  # speed in pixels/sec
 
 var velocity = Vector2.ZERO
 
+
 func _get_input():
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed('right'):
 		velocity.x += 1
+		$StillnessTimer.start()
 	if Input.is_action_pressed('left'):
 		velocity.x -= 1
+		$StillnessTimer.start()
 	if Input.is_action_pressed('down'):
 		velocity.y += 1
+		$StillnessTimer.start()
 	if Input.is_action_pressed('up'):
 		velocity.y -= 1
+		$StillnessTimer.start()
+		
 	# Make sure diagonal movement isn't faster
-
 	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
@@ -26,9 +31,11 @@ func _physics_process(delta):
 
 func _ready():
 	$PlayerCanvasLayer/Watch.visible = false
+	$StillnessTimer.start()
 
 func _input(event):
 	if Input.is_action_just_pressed("check_watch"):
 		$PlayerCanvasLayer/Watch.visible = true
+		$StillnessTimer.start()
 	if Input.is_action_just_released("check_watch"):
 		$PlayerCanvasLayer/Watch.visible = false
