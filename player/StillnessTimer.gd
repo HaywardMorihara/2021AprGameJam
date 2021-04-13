c924bea30176410e731049d34d9d85d2518fa7d6
HaywardMorihara <nathanielmorihara@gmail.com>
2021-04-13 08:28:16 -0400

    Parent interior house type

--- player/StillnessTimer.gd
+++ player/StillnessTimer.gd
@@ -3,10 +3,10 @@ extends Timer
 signal stillness_achieved
 
 
-# For debugging
-func _process(delta)->void:
-	if time_left > 0:
-		print(time_left)
+## For debugging
+#func _process(delta)->void:
+#	if time_left > 0:
+#		print(time_left)
 
 func _on_StillnessTimer_timeout():
 	emit_signal("stillness_achieved")
