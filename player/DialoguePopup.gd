71764dd572f4cb8d566a2a06ec851a8d345a65af
HaywardMorihara <nathanielmorihara@gmail.com>
2021-04-23 07:40:14 -0400

    Animate Dialogue

--- player/DialoguePopup.gd
+++ player/DialoguePopup.gd
@@ -1,8 +1,12 @@
 extends Popup
 
+func _ready()->void:
+	$ProceedLabel.visible = false
+
 func popup(bounds: Rect2 = Rect2( 0, 0, 0, 0 ))->void:
 	.popup(bounds)
 	get_tree().paused = true
+	$AnimationPlayer.play("ShowDialogue")
 
 func _input(event)->void:
 	if event is InputEventKey:
