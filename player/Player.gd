7035c5d8ccb281fcdeb1884795058579edaf9519
HaywardMorihara <nathanielmorihara@gmail.com>
2021-04-25 09:15:09 -0400

    Simplify the animation for now

--- player/Player.gd
+++ player/Player.gd
@@ -23,47 +23,47 @@ func _ready():
 	
 
 func _get_input():
-	velocity = Vector2.ZERO
-	if Input.is_action_pressed('right'):
-		velocity.x += 1
-		$StillnessTimer.start()
-	if Input.is_action_pressed('left'):
-		velocity.x -= 1
-		$StillnessTimer.start()
-	if Input.is_action_pressed('down'):
-		velocity.y += 1
-		$StillnessTimer.start()
-	if Input.is_action_pressed('up'):
-		velocity.y -= 1
-		$StillnessTimer.start()
-	
-	if Input.is_action_just_released("sprint"):
-		require_sprint_release = false
-	
-	var speed : float
-	if is_outside and Input.is_action_pressed('sprint') and $PlayerCanvasLayer/SprintBar.value > 2 and not require_sprint_release:
-		speed = sprint_speed
-		$PlayerAnimation.speed_scale = 2
-		$PlayerCanvasLayer/SprintBar.value -= 2
-		if $Camera2D.zoom.x > 0.5 and $Camera2D.zoom.y > 0.5:
-			$Camera2D.zoom -= Vector2(0.002, 0.002)
-		if $Light2D.scale.x > 15 and $Light2D.scale.y > 15:
-			$Light2D.scale -= Vector2(0.07,0.07)
-	else:
-		speed = walk_speed
-		$PlayerAnimation.speed_scale = 1
-		if $PlayerCanvasLayer/SprintBar.value < 100:
-			$PlayerCanvasLayer/SprintBar.value += 1
-			
-	if $PlayerCanvasLayer/SprintBar.value <= 2:
-		require_sprint_release = true
-	
-	# Make sure diagonal movement isn't faster
-	velocity = velocity.normalized() * speed
-	
-	# TODO Probably should have this somehwere else...but I think it needs to
-	# be part of _physics_process for optmization
 	if not is_in_animation:
+		velocity = Vector2.ZERO
+		if Input.is_action_pressed('right'):
+			velocity.x += 1
+			$StillnessTimer.start()
+		if Input.is_action_pressed('left'):
+			velocity.x -= 1
+			$StillnessTimer.start()
+		if Input.is_action_pressed('down'):
+			velocity.y += 1
+			$StillnessTimer.start()
+		if Input.is_action_pressed('up'):
+			velocity.y -= 1
+			$StillnessTimer.start()
+		
+		if Input.is_action_just_released("sprint"):
+			require_sprint_release = false
+		
+		var speed : float
+		if is_outside and Input.is_action_pressed('sprint') and $PlayerCanvasLayer/SprintBar.value > 2 and not require_sprint_release:
+			speed = sprint_speed
+			$PlayerAnimation.speed_scale = 2
+			$PlayerCanvasLayer/SprintBar.value -= 2
+			if $Camera2D.zoom.x > 0.5 and $Camera2D.zoom.y > 0.5:
+				$Camera2D.zoom -= Vector2(0.002, 0.002)
+			if $Light2D.scale.x > 15 and $Light2D.scale.y > 15:
+				$Light2D.scale -= Vector2(0.07,0.07)
+		else:
+			speed = walk_speed
+			$PlayerAnimation.speed_scale = 1
+			if $PlayerCanvasLayer/SprintBar.value < 100:
+				$PlayerCanvasLayer/SprintBar.value += 1
+				
+		if $PlayerCanvasLayer/SprintBar.value <= 2:
+			require_sprint_release = true
+		
+		# Make sure diagonal movement isn't faster
+		velocity = velocity.normalized() * speed
+		
+		# TODO Probably should have this somehwere else...but I think it needs to
+		# be part of _physics_process for optmization
 		if velocity.length() == 0:
 			$PlayerAnimation.animation = "still_down"
 		elif abs(velocity.x) >= abs(velocity.y):
