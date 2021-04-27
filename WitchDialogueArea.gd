b3e859474ae0fe11b050af322cf3a642b573624f
HaywardMorihara <nathanielmorihara@gmail.com>
2021-04-27 08:18:04 -0400

    Fill out player town a bit

--- WitchDialogueArea.gd
+++ WitchDialogueArea.gd
@@ -7,7 +7,7 @@ func determine_dialogue()->Array:
 			"Hello stranger!",
 			"Curse your daughter?? I've done no such thing! What do you take me for, a witch??",
 			"Please, spend 10 SECONDS with me! It gets so lonely in these woods! Just 10 SECONDS OF STILLNESS",
-			"It doesn't count if you're scurrying around or checking the time!"]
+			"It does NOT count if you're MOVING or CHECKING THE TIME!"]
 	else:
 		return ["Go heal your daughter! Just spend 10 SECONDS with her! That's all it takes! 10 SECONDS OF STILLNESS",
-		"You should try it yourself! STAY STILL FOR 10 SECONDS - you may find that you're able to SEE MORE CLEARLY in those woods!"]
+		"You should try it yourself! STAY STILL FOR 10 SECONDS - you may find that you're able to see more clearly in those woods!"]
