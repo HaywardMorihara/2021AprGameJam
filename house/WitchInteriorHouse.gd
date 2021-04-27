e6138afdbd8e94547e6a020460e3a7858cd1fa71
HaywardMorihara <nathanielmorihara@gmail.com>
2021-04-26 21:25:04 -0400

    Woods hint

--- house/WitchInteriorHouse.gd
+++ house/WitchInteriorHouse.gd
@@ -18,4 +18,4 @@ func _player_stillness_achieved()->void:
 		Global.player_position_before_cutscene = $Player.global_position
 		# I don't think we're doing cutscenes like this...an animation would be ideal
 #		get_tree().change_scene("res://cutscene/WitchHealedCutscene.tscn")
-		$Player.animation_dialogue("Witch", ["Thank you!"])
+		$Player.animation_dialogue("Witch", ["Ahhh that's much better! Thank you very much for your time!"])
