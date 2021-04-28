07f6ce0bc00393e7a8ba6b6f4ac4535735fd325d
HaywardMorihara <nathanielmorihara@gmail.com>
2021-04-28 07:50:32 -0400

    Fix doctor typo

--- npc/DoctorDialogueArea.gd
+++ npc/DoctorDialogueArea.gd
@@ -4,7 +4,7 @@ extends "res://npc/DialogueArea.gd"
 func determine_dialogue()->Array:
 	if not Global.has_player_healed_witch:
 		return [
-			"Your daughter is cursed! It's that WITCH to the NORTH! She's cursed here!",
+			"Your daughter is cursed! It's that WITCH to the NORTH! She's cursed her!",
 			"You'll have to venture to the woods NORTH of here! Find the witch and make her reverse the curse to save your daughter!",
 			"Keep your eye on the TIME! I don't think your daughter has more than 5 MINUTES LEFT!",
 			"HURRY HURRY HURRY!"
